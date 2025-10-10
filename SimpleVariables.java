156d542b97ab6274807281936ff95e41169172f3
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-10 23:55:23 +0530

    new added

---
+++ SimpleVariables.java
@@ -0,0 +1,13 @@
+class SimpleVariables {
+    public static void main(String[] args){
+        String name = "Abdul Samad";
+        String course = "Bachelors in Computer Application";
+        int age = 20;
+
+        System.out.println("Hello "+name);
+        System.out.println("Your course is "+course);
+        System.out.print("And your age is "+age);
+
+
+    }
+}
