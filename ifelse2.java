e1fde40cec2c9c7684b70c9c266bb63e89427362
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-10-11 23:22:57 +0530

    more on conditionals

---
+++ ifelse2.java
@@ -0,0 +1,22 @@
+public class ifelse2{
+    public static void main (String args[]){
+        int n = 20;
+        if(n > 40){
+        System.out.println("You are old");
+        }
+        else
+        System.out.println("You are Young");
+    }
+    // public static void main (String args[]){
+    //     int marks = 78, Sport_marks = 43;
+    //     if(marks < 75){ 
+    //         if(Sport_marks > 50){
+    //         System.out.println("You are a good astudent");
+    //         }
+    //         else
+    //         System.out.println("You need to improve sport");
+    //     }
+    //     else
+    //     System.out.println("You are a weak student");
+    // }
+}
